2788b0c6c9ceb2aa32d222ca8b6b233ced5a08d9
moof mayeda <moofmayeda@yahoo.com>
2014-10-09 10:25:38 -0700

    move site name, logo, and slogan into navbar

--- sites/all/themes/moofstrap/page.tpl.php
+++ sites/all/themes/moofstrap/page.tpl.php
@@ -77,31 +77,7 @@
 
     <div id="header"><div class="section clearfix">
 
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      <?php endif; ?>
 
-      <?php if ($site_name || $site_slogan): ?>
-        <div id="name-and-slogan">
-          <?php if ($site_name): ?>
-            <?php if ($title): ?>
-              <div id="site-name"><strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong></div>
-            <?php else: /* Use h1 when the content title is empty */ ?>
-              <h1 id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </h1>
-            <?php endif; ?>
-          <?php endif; ?>
-
-          <?php if ($site_slogan): ?>
-            <div id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-        </div> <!-- /#name-and-slogan -->
-      <?php endif; ?>
 
       <?php print render($page['header']); ?>
 
@@ -109,6 +85,32 @@
 
     <?php if ($main_menu || $secondary_menu): ?>
       <div id="navigation" class="navbar navbar-default" role="navigation"><div class="section container-fluid">
+        <div class="navbar-header">
+          <?php if ($logo): ?>
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+              <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+            </a>
+          <?php endif; ?>
+          <?php if ($site_name || $site_slogan): ?>
+            <div id="name-and-slogan">
+              <?php if ($site_name): ?>
+                <?php if ($title): ?>
+                  <div id="site-name"><strong>
+                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+                  </strong></div>
+                <?php else: /* Use h1 when the content title is empty */ ?>
+                  <h1 id="site-name">
+                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+                  </h1>
+                <?php endif; ?>
+              <?php endif; ?>
+
+              <?php if ($site_slogan): ?>
+                <div id="site-slogan"><h3><?php print $site_slogan; ?></h3></div>
+              <?php endif; ?>
+            </div> <!-- /#name-and-slogan -->
+          <?php endif; ?>
+        </div>
         <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix', 'nav', 'navbar-nav')))); ?>
         <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix', 'nav', 'navbar-nav', 'navbar-right')))); ?>
       </div></div> <!-- /.section, /#navigation -->
